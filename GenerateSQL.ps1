120aa133c531a890c4a39a61de19fc22fc911b51
yosuke.imai.00 <yosuke.imai.00@gmail.com>
2022-08-11 22:45:34 +0900

    Increased execution speed

--- GenerateSQL.ps1
+++ GenerateSQL.ps1
@@ -10,13 +10,16 @@ $output_file = $directory_name + "\" + $o_file_name
 $table_name = $file_name -replace $extesion,""
 
 [char]$delimiter = "`t"
-[string]$line_feed_code = "`r`n"
+[string]$line_feed_code = "`n"
 
 $header_interval = 10000
 $write_interval = 10000
 
 
-$sql_text = ""
+$sb = New-Object System.Text.StringBuilder
+
+# Clear file content
+$null | Out-File $output_file -Encoding utf8
 
 $line_count = 0
 Get-Content $input_file -Encoding UTF8 | ForEach-Object{
@@ -26,28 +29,26 @@ Get-Content $input_file -Encoding UTF8 | ForEach-Object{
     $sql_value = $sql_value -replace "''","null"
 
     if($line_count -eq 0){
-        $sql_text | Out-File $output_file -Encoding utf8
-
         $joined_header = $data -replace $delimiter,","
         $sql_header = "INSERT INTO @table_name (@header) VALUES"
         $sql_header = $sql_header -replace "@table_name",$table_name -replace "@header", $joined_header
-        $sql_text = $sql_header
+        [void]$sb.Append($sql_header)
     }
     ElseIf($line_count -eq 1){
-        $sql_text = $sql_text + $line_feed_code + $sql_value
+        [void]$sb.Append($line_feed_code + $sql_value)
     }ElseIf($header_interval -ne 0 -and ($line_count % $header_interval) -eq 0){
-        $sql_text = $sql_text + ";" + $line_feed_code + $sql_header + $line_feed_code + $sql_value
+        [void]$sb.Append(";" + $line_feed_code + $sql_header + $line_feed_code + $sql_value)
     }Else{
-        $sql_text = $sql_text + "," +  $line_feed_code + $sql_value
+        [void]$sb.Append("," +  $line_feed_code + $sql_value)
     }
 
     $line_count++
 
     if(($line_count % $write_interval) -eq 0){
 
-        $sql_text | Out-File $output_file -Encoding utf8 -Append -NoClobber
-        $sql_text = ""
+        $sb.ToString() | Out-File $output_file -Encoding utf8 -Append -NoClobber
+        $sb.Length = 0
     }
 }
 
-$sql_text = $sql_text + "`n;" | Out-File $output_file -Encoding utf8 -Append -NoClobber
+$sb.Append($line_feed_code + ";").ToString() | Out-File $output_file -Encoding utf8 -Append -NoClobber
