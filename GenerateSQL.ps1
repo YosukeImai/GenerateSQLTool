07774ab5ee99587cd4b39215e7cc44ff8817562f
yosuke.imai.00 <yosuke.imai.00@gmail.com>
2022-08-22 22:13:56 +0900

    Supprted multiple args

--- GenerateSQL.ps1
+++ GenerateSQL.ps1
@@ -1,54 +1,69 @@
-$input_file = $args[0]
+function GenerateSQLFile {
+    param (
+        $input_file
+    )
 
-# Get output file path
-$directory_name = (Get-item $input_file).DirectoryName
-$file_name = (Get-item $input_file).Name
-$extesion = [System.IO.Path]::GetExtension("$input_file")
-$o_file_name = $file_name -replace $extesion,".sql"
-$output_file = $directory_name + "\" + $o_file_name
+    # Started to generate SQL
+    Write-Output "Started generating SQL:$input_file"
+    # Get output file path
+    $directory_name = (Get-item $input_file).DirectoryName
+    $file_name = (Get-item $input_file).Name
+    $extesion = [System.IO.Path]::GetExtension("$input_file")
+    $o_file_name = $file_name -replace $extesion,".sql"
+    $output_file = $directory_name + "\" + $o_file_name
 
-$table_name = $file_name -replace $extesion,""
+    $table_name = $file_name -replace $extesion,""
 
-[char]$delimiter = "`t"
-[string]$line_feed_code = "`n"
+    [char]$delimiter = "`t"
+    [string]$line_feed_code = "`n"
 
-$header_interval = 10000
-$write_interval = 10000
+    $header_interval = 10000
+    $write_interval = 10000
 
 
-$sb = New-Object System.Text.StringBuilder
+    $sb = New-Object System.Text.StringBuilder
 
-# Clear file content
-$null | Out-File $output_file -Encoding utf8
+    # Clear file content
+    $null | Out-File $output_file -Encoding utf8
 
-$line_count = 0
-Get-Content $input_file -Encoding UTF8 | ForEach-Object{
-    $data = $_
-    $joined_value = $data -replace ",","\," -replace "'","\'" -replace $delimiter,"','"
-    $sql_value = "('" + $joined_value + "')" 
-    $sql_value = $sql_value -replace "''","null"
+    $line_count = 0
+    Get-Content $input_file -Encoding UTF8 | ForEach-Object{
+        $data = $_
+        $joined_value = $data -replace ",","\," -replace "'","\'" -replace $delimiter,"','"
+        $sql_value = "('" + $joined_value + "')" 
+        $sql_value = $sql_value -replace "''","null"
 
-    if($line_count -eq 0){
-        $joined_header = $data -replace $delimiter,","
-        $sql_header = "INSERT INTO @table_name (@header) VALUES"
-        $sql_header = $sql_header -replace "@table_name",$table_name -replace "@header", $joined_header
-        [void]$sb.Append($sql_header)
+        if($line_count -eq 0){
+            $joined_header = $data -replace $delimiter,","
+            $sql_header = "INSERT INTO @table_name (@header) VALUES"
+            $sql_header = $sql_header -replace "@table_name",$table_name -replace "@header", $joined_header
+            [void]$sb.Append($sql_header)
+        }
+        ElseIf($line_count -eq 1){
+            [void]$sb.Append($line_feed_code + $sql_value)
+        }ElseIf($header_interval -ne 0 -and ($line_count % $header_interval) -eq 0){
+            [void]$sb.Append(";" + $line_feed_code + $sql_header + $line_feed_code + $sql_value)
+        }Else{
+            [void]$sb.Append("," +  $line_feed_code + $sql_value)
+        }
+
+        $line_count++
+
+        if(($line_count % $write_interval) -eq 0){
+
+            $sb.ToString() | Out-File $output_file -Encoding utf8 -Append -NoClobber
+            $sb.Length = 0
+        }
     }
-    ElseIf($line_count -eq 1){
-        [void]$sb.Append($line_feed_code + $sql_value)
-    }ElseIf($header_interval -ne 0 -and ($line_count % $header_interval) -eq 0){
-        [void]$sb.Append(";" + $line_feed_code + $sql_header + $line_feed_code + $sql_value)
-    }Else{
-        [void]$sb.Append("," +  $line_feed_code + $sql_value)
-    }
 
-    $line_count++
+    $sb.Append($line_feed_code + ";").ToString() | Out-File $output_file -Encoding utf8 -Append -NoClobber   
 
-    if(($line_count % $write_interval) -eq 0){
-
-        $sb.ToString() | Out-File $output_file -Encoding utf8 -Append -NoClobber
-        $sb.Length = 0
-    }
+    Write-Output "Finished generating SQL:$input_file"
 }
 
-$sb.Append($line_feed_code + ";").ToString() | Out-File $output_file -Encoding utf8 -Append -NoClobber
+foreach ($arg in $args){
+    GenerateSQLFile -input_file $arg
+}
+
+
+
